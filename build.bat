ad9e9d860a8ce883acffd0057a5a3f096b4172ba
Pavel Dzunovich <pavel_dzunovich@epam.com>
2016-03-27 20:26:40 +0300

    Electron build

---
+++ build.bat
@@ -0,0 +1,25 @@
+:: Prerequisites to execute this file: Node.js and Dart SDK
+
+:: Clean
+call rmdir build /S /Q
+call rmdir electron_build /S /Q
+
+:: Resolve dependencies
+call npm install -g electron-packager
+call pub get
+
+:: Compile Dart application
+call pub build
+
+:: Create Electron build folder
+call mkdir electron_build
+
+:: Copy sources for Electron application
+call xcopy electron\* electron_build /s
+call xcopy build\web\* electron_build /s
+
+:: Build Electron application
+cd electron_build
+call npm install
+call electron-packager . --platform=win32 --arch=all
+cd ../
